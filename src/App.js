7f9ae08e035635089b8db277e94a7a4036e51ae1
Jesse Riley <jriley32@student.gsu.edu>
2021-11-28 01:24:12 -0500

    Style search bar, navigation bar, and logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react'
 import { useState, useRef } from 'react';
+import logo from './Logo_LightBG.png';
+import account from './account.png';
 
 function App() {
   const [inputs, setInputs] = useState([]);
@@ -47,55 +49,104 @@ function App() {
   }
   return (
 
-    <div class="divClass">
-      <header>
-        <div class="logo">FORK-IT-OVER</div>
-        <nav>
-          <ul class="links">
-            <li><a href="#">My Account</a></li>
-            <li><a href="#">My Favorites</a></li>
-            <li><a> <form id="logout" method="POST" action="/logout">
-              <div class="inputs">
-                <input id="submit" type="submit" value="LOGOUT" />
+    <div>
+      <div class="header">
+        <div class="header-container">
+          <div class="header-content">
+            <div class="logo-container"><img src={logo} class="logo" alt="logo" /></div>
+            <div class="searchbar-outer-container">
+              <div class="searchbar-mid-container">
+                <div class="searchbar-inner-container">
+                  <div class="searchbar">
+                    <div class="searchbar-table">
+                      <div class="searchbar-input">
+                        <div class="searchbar-input-flex">
+                          <div class="food-type-query-container">
+                            <div class="food-type-query">
+                              <input ref={foodInput} placeholder="ramen, cheap dinner, Thai" type="text" name="food-type-query" required class="textbox"
+                                spellcheck="value" data-testid="query_input" autocomplete="off" aria-autocomplete="list" tabindex="0" id="food-type-query" />
+                            </div>
+                          </div>
+                          <div class="location-container">
+                            <div class="location">
+                              <input ref={addressInput} placeholder="address, neighborhood, city, state or zip" type="text" name="address"
+                                required class="textbox" spellcheck="value" data-testid="location_input" autocomplete="off"
+                                aria-autocomplete="list" tabindex="0" />
+                            </div>
+                          </div>
+                          <div class="radius-container">
+                            <div class="radius">
+                              <input ref={radiusInput} placeholder="radius in miles" class="radius-select" type="number" name="radius" min="1" max="20" required />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="searchbar-button-outer-container">
+                        <div class="searchbar-button-mid-container">
+                          <div class="searchbar-button-inner-container">
+                            <button class="searchbar-button" onClick={onSavedUser}>*</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-
-            </form></a></li>
-          </ul>
-        </nav>
-
-      </header>
-      <h4>Search the best food places available near you!</h4>
-
-      <input ref={foodInput} placeholder="Enter the food/cuisine" type="text" name="food" required class="textbox"
-        spellcheck="value" data-testid="query_input" />
-      <input ref={addressInput} placeholder="Enter the location" type="text" name="address" required class="textbox" spellcheck="value" data-testid="location_input" />
-      <select class="textbox" name="radius" ref={radiusInput} data-testid="radius_options">
-        <option value="" disabled selected hidden>Enter the radius in miles</option>
-        <option value="5" data-testid="5_miles">5</option>
-        <option value="10" data-testid="10_miles">10</option>
-        <option value="15" data-testid="15_miles">15</option>
-      </select>
-      <button onClick={onSavedUser}>Search!</button>
-      <h1 id="message" data-testid="loading">{message}</h1>
-
-      {result?.flaskData?.businesses &&
-        result.flaskData.businesses.sort((a, b) => b.rating - a.rating).map((d) => (
-          <div key={d.id} class="places">
-            <h2>{d.name}</h2>
-            <h4>Rating: {d.rating}</h4>
-            <p><a target="_blank" rel="noopener noreferrer" href={d.url}>Website</a></p>
-            <p>Address: {d?.location?.display_address}</p>
-            <img id="images" alt="Restaurant image from Yelp" src={d.image_url} />
-            <div class="grid_container">
-              {d?.delivery_services?.UberEats !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.UberEats}>Uber Eats</a></p>}
-              {d?.delivery_services?.Grubhub !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.Grubhub}>Grubhub</a></p>}
-              {d?.delivery_services?.Postmates !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.Postmates}>Postmates</a></p>}
-              {d?.delivery_services?.DoorDash !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.DoorDash}>DoorDash</a></p>}
             </div>
-
+            <div class="navbar">
+              <span class="navbar-link-container">
+                <a href="#" class="navbar-link">
+                  <div class="navbar-link-title-padding">
+                    <div class="navbar-link-title-container">
+                      <p class="navbar-link-title">
+                        About
+                      </p>
+                    </div>
+                  </div>
+                </a>
+              </span>
+            </div>
+            <div class="account-outer-container">
+              <div class="account-table">
+                <div class="account-table-cell">
+                  <div class="account-mid-container">
+                    <div class="account-inner-container">
+                      <button class="account" aria-label="Toggle Menu" aria-haspopup="menu" aria-controls="header-dropdown-menu"
+                        aria-expanded="false" type="submit">
+                        <span class="account-image-outer-wrapper">
+                          <span class="account-image-inner-wrapper">
+                            <img class="account-image" src={account} alt="Account" width="36" height="36" />
+                          </span>
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        ))
-      }
+        </div>
+      </div>
+      <div class="results">
+        <h1 id="message" data-testid="loading">{message}</h1>
+        {result?.flaskData?.businesses &&
+          result.flaskData.businesses.sort((a, b) => b.rating - a.rating).map((d) => (
+            <div key={d.id} class="places">
+              <h2>{d.name}</h2>
+              <h4>Rating: {d.rating}</h4>
+              <p><a target="_blank" rel="noopener noreferrer" href={d.url}>Website</a></p>
+              <p>Address: {d?.location?.display_address}</p>
+              <img id="images" alt="Restaurant image from Yelp" src={d.image_url} />
+              <div class="grid_container">
+                {d?.delivery_services?.UberEats !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.UberEats}>Uber Eats</a></p>}
+                {d?.delivery_services?.Grubhub !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.Grubhub}>Grubhub</a></p>}
+                {d?.delivery_services?.Postmates !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.Postmates}>Postmates</a></p>}
+                {d?.delivery_services?.DoorDash !== undefined && <p class="grid_item"><a target="_blank" rel="noopener noreferrer" href={d.delivery_services.DoorDash}>DoorDash</a></p>}
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
