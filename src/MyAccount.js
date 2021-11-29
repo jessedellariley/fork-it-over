62a43ac1d9e8f52b4ebd7648492f1895ba9be15b
ahmedfarah413 <ahmedfarah413@gmail.com>
2021-11-29 15:03:30 -0500

    Made a mistake creating the address and vacation part of the file

--- src/MyAccount.js
+++ src/MyAccount.js
@@ -7,6 +7,7 @@ function MyAccount() {
   const onUpdate = async () => {
     console.log("onUpdate");
   };
+
   return (
     <div>
       <div className="topnav">
@@ -20,8 +21,40 @@ function MyAccount() {
           <Link to="/logout">Logout</Link>
         </div>
       </div>
+      <div className="container myacccount-container">
+        <h2 className="header">My Account</h2>
+        <div className="body">
+          <form>
+            <div>
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                placeholder="First Name"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                placeholder="First Name"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">User Name:</label>
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                placeholder="Username"
+              />
+            </div>
+      </div>
     </div>
   );
+}
+
 export default MyAccount;
-
-
